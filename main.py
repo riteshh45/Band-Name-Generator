7da832290d9b260e74ca14e4b8ff0449aa5aeb6d
riteshh45 <135577659+riteshh45@users.noreply.github.com>
2024-12-19 08:47:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello, Welcome is band generator Project!")
+city_name =input("What is your city name?\n")
+pet_name =input("What is your pet name?\n")
+print(" Your band name is " + city_name + " " + pet_name)
